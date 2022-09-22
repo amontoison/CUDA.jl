2d48c720f7ec1b35033453542cbc7480d33cb5b0
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-09-22 02:14:49 -0400

    bug 3803297

--- lib/cusparse/generic.jl
+++ lib/cusparse/generic.jl
@@ -57,11 +57,24 @@ Base.unsafe_convert(::Type{cusparseDnMatDescr_t}, desc::CuDenseMatrixDescriptor)
 mutable struct CuSparseMatrixDescriptor
     handle::cusparseSpMatDescr_t
 
+    function CuSparseMatrixDescriptor(A::CuSparseMatrixCOO, IndexBase::Char)
+        desc_ref = Ref{cusparseSpMatDescr_t}()
+        cusparseCreateCoo(
+            desc_ref,
+            size(A)..., A.nnz,
+            A.rowInd, A.colInd, nonzeros(A),
+            eltype(A.rowInd), IndexBase, eltype(nonzeros(A))
+        )
+        obj = new(desc_ref[])
+        finalizer(cusparseDestroySpMat, obj)
+        return obj
+    end
+
     function CuSparseMatrixDescriptor(A::CuSparseMatrixCSR, IndexBase::Char)
         desc_ref = Ref{cusparseSpMatDescr_t}()
         cusparseCreateCsr(
             desc_ref,
-            size(A)..., length(nonzeros(A)),
+            size(A)..., A.nnz,
             A.rowPtr, A.colVal, nonzeros(A),
             eltype(A.rowPtr), eltype(A.colVal), IndexBase, eltype(nonzeros(A))
         )
@@ -70,21 +83,21 @@ mutable struct CuSparseMatrixDescriptor
         return obj
     end
 
-    function CuSparseMatrixDescriptor(A::CuSparseMatrixCSC, IndexBase::Char; convert=true)
+    function CuSparseMatrixDescriptor(A::CuSparseMatrixCSC, IndexBase::Char; convert=false)
         desc_ref = Ref{cusparseSpMatDescr_t}()
         if convert
             # many algorithms, e.g. mv! and mm!, do not support CSC sparse format
             # so we eagerly convert this to a CSR matrix.
             cusparseCreateCsr(
                 desc_ref,
-                reverse(size(A))..., length(nonzeros(A)),
+                reverse(size(A))..., A.nnz,
                 A.colPtr, rowvals(A), nonzeros(A),
                 eltype(A.colPtr), eltype(rowvals(A)), IndexBase, eltype(nonzeros(A))
             )
         else
             cusparseCreateCsc(
                 desc_ref,
-                size(A)..., length(nonzeros(A)),
+                size(A)..., A.nnz,
                 A.colPtr, rowvals(A), nonzeros(A),
                 eltype(A.colPtr), eltype(rowvals(A)), IndexBase, eltype(nonzeros(A))
             )
@@ -109,8 +122,10 @@ function gather!(X::CuSparseVector, Y::CuVector, index::SparseChar)
     X
 end
 
-function mv!(transa::SparseChar, alpha::Number, A::Union{CuSparseMatrixBSR{TA},CuSparseMatrixCSR{TA}},
-             X::DenseCuVector{T}, beta::Number, Y::DenseCuVector{T}, index::SparseChar, algo::cusparseSpMVAlg_t=CUSPARSE_MV_ALG_DEFAULT) where {TA, T}
+function mv!(transa::SparseChar, alpha::Number, A::Union{CuSparseMatrixCSC{TA},CuSparseMatrixCSR{TA},CuSparseMatrixCOO{TA}}, X::DenseCuVector{T},
+             beta::Number, Y::DenseCuVector{T}, index::SparseChar, algo::cusparseSpMVAlg_t=CUSPARSE_MV_ALG_DEFAULT) where {TA, T}
+
+    descA = CuSparseMatrixDescriptor(A, index)
     m,n = size(A)
 
     if transa == 'N'
@@ -119,7 +134,6 @@ function mv!(transa::SparseChar, alpha::Number, A::Union{CuSparseMatrixBSR{TA},C
         chkmvdims(X,m,Y,n)
     end
 
-    descA = CuSparseMatrixDescriptor(A, index)
     descX = CuDenseVectorDescriptor(X)
     descY = CuDenseVectorDescriptor(Y)
 
@@ -144,58 +158,13 @@ function mv!(transa::SparseChar, alpha::Number, A::Union{CuSparseMatrixBSR{TA},C
         cusparseSpMV(handle(), transa, Ref{compute_type}(alpha), descA, descX, Ref{compute_type}(beta),
                      descY, compute_type, algo, buffer)
     end
-    Y
-end
-
-function mv!(transa::SparseChar, alpha::Number, A::CuSparseMatrixCSC{TA}, X::DenseCuVector{T},
-             beta::Number, Y::DenseCuVector{T}, index::SparseChar, algo::cusparseSpMVAlg_t=CUSPARSE_MV_ALG_DEFAULT) where {TA, T}
-    ctransa = 'N'
-    if transa == 'N'
-        ctransa = 'T'
-    elseif transa == 'C' && TA <: Complex
-        throw(ArgumentError("Matrix-vector multiplication with the adjoint of a CSC matrix" *
-                            " is not supported. Use a CSR matrix instead."))
-    end
-
-    n,m = size(A)
-
-    if ctransa == 'N'
-        chkmvdims(X,n,Y,m)
-    elseif ctransa == 'T' || ctransa == 'C'
-        chkmvdims(X,m,Y,n)
-    end
-
-    descA = CuSparseMatrixDescriptor(A, index)
-    descX = CuDenseVectorDescriptor(X)
-    descY = CuDenseVectorDescriptor(Y)
-
-    # operations with 16-bit numbers always imply mixed-precision computation
-    # TODO: we should better model the supported combinations here,
-    #       and error if using an unsupported one (like with gemmEx!)
-    compute_type = if version() >= v"11.4" && T == Float16
-        Float32
-    elseif version() >= v"11.7.2" && T == ComplexF16
-        ComplexF32
-    else
-        T
-    end
-
-    function bufferSize()
-        out = Ref{Csize_t}()
-        cusparseSpMV_bufferSize(handle(), ctransa, Ref{compute_type}(alpha), descA, descX, Ref{compute_type}(beta),
-                                descY, compute_type, algo, out)
-        return out[]
-    end
-    with_workspace(bufferSize) do buffer
-        cusparseSpMV(handle(), ctransa, Ref{compute_type}(alpha), descA, descX, Ref{compute_type}(beta),
-                     descY, compute_type, algo, buffer)
-    end
-
     return Y
 end
 
-function mm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::CuSparseMatrixCSR{T},
+function mm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::Union{CuSparseMatrixCSC{T},CuSparseMatrixCSR{T},CuSparseMatrixCOO{T}},
              B::DenseCuMatrix{T}, beta::Number, C::DenseCuMatrix{T}, index::SparseChar, algo::cusparseSpMMAlg_t=CUSPARSE_MM_ALG_DEFAULT) where {T}
+
+    descA = CuSparseMatrixDescriptor(A, index)
     m,k = size(A)
     n = size(C)[2]
 
@@ -209,7 +178,6 @@ function mm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::CuSparseM
         chkmmdims(B,C,n,m,k,n)
     end
 
-    descA = CuSparseMatrixDescriptor(A, index)
     descB = CuDenseMatrixDescriptor(B)
     descC = CuDenseMatrixDescriptor(C)
 
@@ -225,50 +193,6 @@ function mm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::CuSparseM
             handle(), transa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
             descC, T, algo, buffer)
     end
-
-    return C
-end
-
-function mm!(transa::SparseChar, transb::SparseChar, alpha::Number, A::CuSparseMatrixCSC{T},
-             B::DenseCuMatrix{T}, beta::Number, C::DenseCuMatrix{T}, index::SparseChar, algo::cusparseSpMMAlg_t=CUSPARSE_MM_ALG_DEFAULT) where {T}
-    ctransa = 'N'
-    if transa == 'N'
-        ctransa = 'T'
-    elseif transa == 'C' && T <: Complex
-        throw(ArgumentError("Matrix-matrix multiplication with the adjoint of a CSC matrix" *
-                            " is not supported. Use a CSR matrix instead."))
-    end
-
-    k,m = size(A)
-    n = size(C)[2]
-
-    if ctransa == 'N' && transb == 'N'
-        chkmmdims(B,C,k,n,m,n)
-    elseif ctransa == 'N' && transb != 'N'
-        chkmmdims(B,C,n,k,m,n)
-    elseif ctransa != 'N' && transb == 'N'
-        chkmmdims(B,C,m,n,k,n)
-    elseif ctransa != 'N' && transb != 'N'
-        chkmmdims(B,C,n,m,k,n)
-    end
-
-    descA = CuSparseMatrixDescriptor(A, index)
-    descB = CuDenseMatrixDescriptor(B)
-    descC = CuDenseMatrixDescriptor(C)
-
-    function bufferSize()
-        out = Ref{Csize_t}()
-        cusparseSpMM_bufferSize(
-            handle(), ctransa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
-            descC, T, algo, out)
-        return out[]
-    end
-    with_workspace(bufferSize) do buffer
-        cusparseSpMM(
-            handle(), ctransa, transb, Ref{T}(alpha), descA, descB, Ref{T}(beta),
-            descC, T, algo, buffer)
-    end
-
     return C
 end
 
@@ -345,3 +269,103 @@ function mm!(transa::SparseChar, transb::SparseChar, α::Number, A::CuSparseMatr
     end
 end
 
+function sv!(transa::SparseChar, uplo::SparseChar, diag::SparseChar,
+             alpha::Number, A::Union{CuSparseMatrixCSC{T},CuSparseMatrixCSR{T},CuSparseMatrixCOO{T}}, X::DenseCuVector{T},
+             Y::DenseCuVector{T}, index::SparseChar, algo::cusparseSpSVAlg_t=CUSPARSE_SPSV_ALG_DEFAULT) where {T}
+
+    if isa(A, CuSparseMatrixCSC) && transa == 'C' && T <: Complex
+        throw(ArgumentError("Backward and forward sweeps with the adjoint of a CSC matrix is not supported. Use a CSR or COO matrix instead."))
+    end
+
+    mA,nA = size(A)
+    mX = length(X)
+    mY = length(Y)
+
+    (mA != nA) && throw(DimensionMismatch("A must be square, but has dimensions ($mA,$nA)!"))
+    (mX != mA) && throw(DimensionMismatch("X must have length $mA, but has length $mX"))
+    (mY != mA) && throw(DimensionMismatch("Y must have length $mA, but has length $mY"))
+
+    if isa(A, CuSparseMatrixCSC)
+        # cusparseSpSV doesn't supports CSC format
+        descA = CuSparseMatrixDescriptor(A, index, convert=true)
+        transa2 = transa == 'N' ? 'T' : 'N'
+        uplo2 = uplo == 'U' ? 'L' : 'U'
+    else
+        descA = CuSparseMatrixDescriptor(A, index)
+        transa2 = transa
+        uplo2 = uplo
+    end
+
+    cusparse_uplo = Ref{cusparseFillMode_t}(uplo2)
+    cusparse_diag = Ref{cusparseDiagType_t}(diag)
+
+    cusparseSpMatSetAttribute(descA, 'F', cusparse_uplo, Csize_t(sizeof(cusparse_uplo)))
+    cusparseSpMatSetAttribute(descA, 'D', cusparse_diag, Csize_t(sizeof(cusparse_diag)))
+
+    descX = CuDenseVectorDescriptor(X)
+    descY = CuDenseVectorDescriptor(Y)
+
+    spsv_desc = CuSparseSpSVDescriptor()
+    function bufferSize()
+        out = Ref{Csize_t}()
+        cusparseSpSV_bufferSize(handle(), transa2, Ref{T}(alpha), descA, descX, descY, T, algo, spsv_desc, out)
+        return out[]
+    end
+    with_workspace(bufferSize) do buffer
+        cusparseSpSV_analysis(handle(), transa2, Ref{T}(alpha), descA, descX, descY, T, algo, spsv_desc, buffer)
+        cusparseSpSV_solve(handle(), transa2, Ref{T}(alpha), descA, descX, descY, T, algo, spsv_desc)
+    end
+    return Y
+end
+
+function sm!(transa::SparseChar, transb::SparseChar, uplo::SparseChar, diag::SparseChar,
+             alpha::Number, A::Union{CuSparseMatrixCSC{T},CuSparseMatrixCSR{T},CuSparseMatrixCOO{T}}, B::DenseCuMatrix{T},
+             C::DenseCuMatrix{T}, index::SparseChar, algo::cusparseSpSMAlg_t=CUSPARSE_SPSM_ALG_DEFAULT) where {T}
+
+    if isa(A, CuSparseMatrixCSC) && transa == 'C' && T <: Complex
+        throw(ArgumentError("Backward and forward sweeps with the adjoint of a CSC matrix is not supported. Use a CSR or COO matrix instead."))
+    end
+
+    mA,nA = size(A)
+    mB,nB = size(B)
+    mC,nC = size(C)
+
+    (mA != nA) && throw(DimensionMismatch("A must be square, but has dimensions ($mA,$nA)!"))
+    (mC != mA) && throw(DimensionMismatch("C must have $mA rows, but has $mC rows"))
+    (mB != mA) && (transb == 'N') && throw(DimensionMismatch("B must have $mA rows, but has $mB rows"))
+    (nB != mA) && (transb != 'N') && throw(DimensionMismatch("B must have $mA columns, but has $nB columns"))
+    (nB != nC) && (transb == 'N') && throw(DimensionMismatch("B and C must have the same number of columns, but B has $nB columns and C has $nC columns"))
+    (mB != nC) && (transb != 'N') && throw(DimensionMismatch("B must have the same the number of rows that C has as columns, but B has $mB rows and C has $nC columns"))
+
+    if isa(A, CuSparseMatrixCSC)
+        # cusparseSpSM doesn't supports CSC format
+        descA = CuSparseMatrixDescriptor(A, index, convert=true)
+        transa2 = transa == 'N' ? 'T' : 'N'
+        uplo2 = uplo == 'U' ? 'L' : 'U'
+    else
+        descA = CuSparseMatrixDescriptor(A, index)
+        transa2 = transa
+        uplo2 = uplo
+    end
+
+    cusparse_uplo = Ref{cusparseFillMode_t}(uplo2)
+    cusparse_diag = Ref{cusparseDiagType_t}(diag)
+
+    cusparseSpMatSetAttribute(descA, 'F', cusparse_uplo, Csize_t(sizeof(cusparse_uplo)))
+    cusparseSpMatSetAttribute(descA, 'D', cusparse_diag, Csize_t(sizeof(cusparse_diag)))
+
+    descB = CuDenseMatrixDescriptor(B)
+    descC = CuDenseMatrixDescriptor(C)
+
+    spsm_desc = CuSparseSpSMDescriptor()
+    function bufferSize()
+        out = Ref{Csize_t}()
+        cusparseSpSM_bufferSize(handle(), transa2, transb, Ref{T}(alpha), descA, descB, descC, T, algo, spsm_desc, out)
+        return out[]
+    end
+    with_workspace(bufferSize) do buffer
+        cusparseSpSM_analysis(handle(), transa2, transb, Ref{T}(alpha), descA, descB, descC, T, algo, spsm_desc, buffer)
+        cusparseSpSM_solve(handle(), transa2, transb, Ref{T}(alpha), descA, descB, descC, T, algo, spsm_desc)
+    end
+    return C
+end
--- test/cusparse/generic.jl
+++ test/cusparse/generic.jl
@@ -23,44 +23,131 @@ end
 
 if CUSPARSE.version() >= v"11.4.1" # lower CUDA version doesn't support these algorithms
 
-    @testset "mm algo=$algo" for algo in [
-        CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT,
-        CUSPARSE.CUSPARSE_SPMM_CSR_ALG1,
-        CUSPARSE.CUSPARSE_SPMM_CSR_ALG2,
-        CUSPARSE.CUSPARSE_SPMM_CSR_ALG3,
-    ]
-        @testset "mm $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
-            A = sprand(T, 10, 10, 0.1)
-            B = rand(T, 10, 2)
-            C = rand(T, 10, 2)
-            dA = CuSparseMatrixCSR(A)
-            dB = CuArray(B)
-            dC = CuArray(C)
+    SPMV_ALGOS = Dict(CuSparseMatrixCSC => [CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT],
+                      CuSparseMatrixCSR => [CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT,
+                                            CUSPARSE.CUSPARSE_SPMV_CSR_ALG1,
+                                            CUSPARSE.CUSPARSE_SPMV_CSR_ALG2],
+                      CuSparseMatrixCOO => [CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT,
+                                            # CUSPARSE.CUSPARSE_SPMV_COO_ALG2,
+                                            CUSPARSE.CUSPARSE_SPMV_COO_ALG1])
 
-            alpha = 1.2
-            beta = 1.3
-            mm!('N', 'N', alpha, dA, dB, beta, dC, 'O', algo)
-            @test alpha * A * B + beta * C ≈ collect(dC)
+    SPMM_ALGOS = Dict(CuSparseMatrixCSC => [CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT],
+                      CuSparseMatrixCSR => [CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT,
+                                            CUSPARSE.CUSPARSE_SPMM_CSR_ALG1,
+                                            CUSPARSE.CUSPARSE_SPMM_CSR_ALG2,
+                                            CUSPARSE.CUSPARSE_SPMM_CSR_ALG3],
+                      CuSparseMatrixCOO => [CUSPARSE.CUSPARSE_SPMM_ALG_DEFAULT,
+                                            CUSPARSE.CUSPARSE_SPMM_COO_ALG1,
+                                            # CUSPARSE.CUSPARSE_SPMM_COO_ALG2,
+                                            CUSPARSE.CUSPARSE_SPMM_COO_ALG3,
+                                            CUSPARSE.CUSPARSE_SPMM_COO_ALG4])
+
+    for SparseMatrixType in [CuSparseMatrixCSC, CuSparseMatrixCSR, CuSparseMatrixCOO]
+        @testset "$SparseMatrixType -- mv! algo=$algo" for algo in SPMV_ALGOS[SparseMatrixType]
+            @testset "mv! $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
+                for (transa, opa) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                    T <: Real && transa == 'C' && continue
+                    A = sprand(T, 10, 10, 0.1)
+                    B = rand(T, 10)
+                    C = rand(T, 10)
+                    dA = SparseMatrixType(A)
+                    dB = CuArray(B)
+                    dC = CuArray(C)
+
+                    alpha = rand(T)
+                    beta = rand(T)
+                    mv!(transa, alpha, dA, dB, beta, dC, 'O', algo)
+                    @test alpha * opa(A) * B + beta * C ≈ collect(dC)
+                end
+            end
+        end
+
+        @testset "$SparseMatrixType -- mm! algo=$algo" for algo in SPMM_ALGOS[SparseMatrixType]
+            @testset "mm! $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
+                for (transa, opa) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                    for (transb, opb) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                        T <: Real && transa == 'C' && continue
+                        T <: Real && transb == 'C' && continue
+                        algo == CUSPARSE.CUSPARSE_SPMM_CSR_ALG3 && (transa != 'N' || transb != 'N') && continue
+                        A = sprand(T, 10, 10, 0.1)
+                        B = transb == 'N' ? rand(T, 10, 2) : rand(T, 2, 10)
+                        C = rand(T, 10, 2)
+                        dA = SparseMatrixType(A)
+                        dB = CuArray(B)
+                        dC = CuArray(C)
+
+                        alpha = rand(T)
+                        beta = rand(T)
+                        mm!(transa, transb, alpha, dA, dB, beta, dC, 'O', algo)
+                        @test alpha * opa(A) * opb(B) + beta * C ≈ collect(dC)
+                    end
+                end
+            end
         end
     end
+end # CUSPARSE.version >= 11.4.1
 
-    @testset "mv algo=$algo" for algo in [
-        CUSPARSE.CUSPARSE_SPMV_ALG_DEFAULT,
-        CUSPARSE.CUSPARSE_SPMV_CSR_ALG1,
-        CUSPARSE.CUSPARSE_SPMV_CSR_ALG2,
-    ]
-        @testset "mv $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
-            A = sprand(T, 10, 10, 0.1)
-            B = rand(T, 10)
-            C = rand(T, 10)
-            dA = CuSparseMatrixCSR(A)
-            dB = CuArray(B)
-            dC = CuArray(C)
+if CUSPARSE.version() >= v"11.5.1"
 
-            alpha = 1.2
-            beta = 1.3
-            mv!('N', alpha, dA, dB, beta, dC, 'O', algo)
-            @test alpha * A * B + beta * C ≈ collect(dC)
+    SPSV_ALGOS = Dict(CuSparseMatrixCSC => [CUSPARSE.CUSPARSE_SPSV_ALG_DEFAULT],
+                      CuSparseMatrixCSR => [CUSPARSE.CUSPARSE_SPSV_ALG_DEFAULT],
+                      CuSparseMatrixCOO => [CUSPARSE.CUSPARSE_SPSV_ALG_DEFAULT])
+
+    SPSM_ALGOS = Dict(CuSparseMatrixCSC => [CUSPARSE.CUSPARSE_SPSM_ALG_DEFAULT],
+                      CuSparseMatrixCSR => [CUSPARSE.CUSPARSE_SPSM_ALG_DEFAULT],
+                      CuSparseMatrixCOO => [CUSPARSE.CUSPARSE_SPSM_ALG_DEFAULT])
+
+    for SparseMatrixType in [CuSparseMatrixCSC, CuSparseMatrixCSR, CuSparseMatrixCOO]
+        @testset "$SparseMatrixType -- sv! algo=$algo" for algo in SPSV_ALGOS[SparseMatrixType]
+            @testset "sv! $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
+                for (transa, opa) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                    SparseMatrixType == CuSparseMatrixCSC && T <: Complex && transa == 'C' && continue
+                    for uplo in ('L', 'U')
+                        for diag in ('U', 'N')
+                            T <: Real && transa == 'C' && continue
+                            A = rand(T, 10, 10)
+                            A = uplo == 'L' ? tril(A) : triu(A)
+                            A = diag == 'U' ? A - Diagonal(A) + I : A
+                            A = sparse(A)
+                            B = rand(T, 10)
+                            C = rand(T, 10)
+                            dA = SparseMatrixType(A)
+                            dB = CuArray(B)
+                            dC = CuArray(C)
+                            alpha = rand(T)
+                            sv!(transa, uplo, diag, alpha, dA, dB, dC, 'O', algo)
+                            @test opa(A) \ (alpha * B) ≈ collect(dC)
+                        end
+                    end
+                end
+            end
+        end
+
+        @testset "$SparseMatrixType -- mv! algo=$algo" for algo in SPSM_ALGOS[SparseMatrixType]
+            @testset "mv! $T" for T in [Float32, Float64, ComplexF32, ComplexF64]
+                for (transa, opa) in [('N', identity), ('T', transpose), ('C', adjoint)]
+                    SparseMatrixType == CuSparseMatrixCSC && T <: Complex && transa == 'C' && continue
+                    for (transb, opb) in [('N', identity), ('T', transpose)]
+                        for uplo in ('L', 'U')
+                            for diag in ('U', 'N')
+                                T <: Real && transa == 'C' && continue
+                                A = rand(T, 10, 10)
+                                A = uplo == 'L' ? tril(A) : triu(A)
+                                A = diag == 'U' ? A - Diagonal(A) + I : A
+                                A = sparse(A)
+                                B = transb == 'N' ? rand(T, 10, 2) : rand(T, 2, 10)
+                                C = rand(T, 10, 2)
+                                dA = SparseMatrixType(A)
+                                dB = CuArray(B)
+                                dC = CuArray(C)
+                                alpha = rand(T)
+                                sm!(transa, transb, uplo, diag, alpha, dA, dB, dC, 'O', algo)
+                                @test opa(A) \ (alpha * opb(B)) ≈ collect(dC)
+                            end
+                        end
+                    end
+                end
+            end
         end
     end
-end # version >= 11.4.1
+end # CUSPARSE.version >= 11.5.1
